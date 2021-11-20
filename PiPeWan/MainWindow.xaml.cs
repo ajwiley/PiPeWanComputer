8636061e9b6e96ca641bd702d88db1a1b6b4d6d0
Aron Wiley <61031954+ajwiley@users.noreply.github.com>
2021-11-20 17:59:06 -0600

    Added bound properties

--- PiPeWan/MainWindow.xaml.cs
+++ PiPeWan/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PiPeWan.HelperClasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,8 +19,11 @@ namespace PiPeWan {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        public static BoundProperties _BoundProperties = new BoundProperties();
+
         public MainWindow() {
             InitializeComponent();
+            DataContext = _BoundProperties;
         }
     }
 }
