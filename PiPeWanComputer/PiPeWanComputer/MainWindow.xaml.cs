36c52fc17e2a3b4478ea3f2b8553aa6d16c94355
Aron Wiley <61031954+ajwiley@users.noreply.github.com>
2021-11-28 01:47:14 -0600

    New program

---
+++ PiPeWanComputer/PiPeWanComputer/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Sockets;
+using System.IO.Ports;
+
+
+namespace PiPeWanComputer {    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private static SerialPort _Port = new();
+        
+        public MainWindow() {
+            InitializeComponent();
+            _Port.Dispose();
+
+            try {
+                _Port = new SerialPort() {
+                    PortName = "COM1",
+                    BaudRate = 9600
+                };
+            }
+            catch {
+                Console.WriteLine("Could not connect to the arduino");
+            }
+        }
+    }
+}
