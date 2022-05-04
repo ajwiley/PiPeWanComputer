a417ac3d57b28951b9f668db1888240a3a00958b
Alex Rossillon <78603922+arosey@users.noreply.github.com>
2022-05-04 17:50:24 -0500

    Styles and Functioning Login

--- PiPeWanComputer/PiPeWanComputer/Login.xaml.cs
+++ PiPeWanComputer/PiPeWanComputer/Login.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,64 +13,65 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using MaterialDesignThemes.Wpf;
+using PiPeWanComputer.SQL_Stuff;
 using PiPeWanComputer.Views;
 
-namespace PiPeWanComputer
-{
+namespace PiPeWanComputer {
     /// <summary>
     /// Interaction logic for Login.xaml
     /// </summary>
-    public partial class Login : Window
-    {
-        public Login()
-        {
+    public partial class Login : Window {
+        public Login() {
             InitializeComponent();
+            PipeDB.CreateDB();
         }
 
-        private void ExitApp(object sender, RoutedEventArgs e)
-        {
+        private void ExitApp(object sender, RoutedEventArgs e) {
             Application.Current.Shutdown();
         }
 
-        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
-        {
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e) {
             base.OnMouseLeftButtonDown(e);
             DragMove();
         }
 
-        private void DoLogin(object sender, RoutedEventArgs e)
-        {
-            string username = txtUsername.Text;
+        private void DoLogin(object sender, RoutedEventArgs e) {
+            string username = txtUsername.Text.ToLower();
             string password = txtPassword.Password;
 
-            MainWindow mainWindow = new MainWindow();
-            mainWindow.Show();
-            Close();
+            password = password.PadRight(64, '0');
+            var pBytes = Encoding.UTF8.GetBytes(password);
 
+            // If no user with that username is fount, SelectUser() returns null
+            User? user = PipeDB.SelectUser(username);
 
-        }
-
-        private void OpenRegisterPage(object sender, RoutedEventArgs e)
-        {
-            Registration registration = new Registration();
-            registration.Show();
-            Close();
+            if (user is null) {
+                // The given username does not exist in the database
+                MessageBox.Show($"No user with username \"{username}\" exists!");
+            }
+            else if (!pBytes.SequenceEqual(user.PasswordHash)) {
+                // The incorrect password.
+                MessageBox.Show($"Incorrect password for \"{username}\"");
+            }
+            else {
+                // let them login
+                MainWindow mainWindow = new MainWindow();
+                Close();
+                mainWindow.Show();
+            }
         }
 
         public bool IsDarkTheme { get; set; }
         private readonly PaletteHelper paletteHelper = new PaletteHelper();
-        private void toggleTheme(object sender, RoutedEventArgs e)
-        {
+        private void toggleTheme(object sender, RoutedEventArgs e) {
             ITheme theme = paletteHelper.GetTheme();
 
-            if(IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
-            {
+            if (IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark) {
                 IsDarkTheme = false;
                 theme.SetBaseTheme(Theme.Light);
             }
-            else
-            {
-                IsDarkTheme=true;
+            else {
+                IsDarkTheme = true;
                 theme.SetBaseTheme(Theme.Dark);
             }
 
